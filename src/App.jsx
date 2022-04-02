6fac6f93b316dab59b1cd899ec9acb2d5d1aa97e
mocrodimocros <82786966+mocrodimocros@users.noreply.github.com>
2022-04-02 09:29:36 +0200

    express CR part + graph schema

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-const initialIssues = [
-    {
-    id: 1, status: 'New', owner: 'Ravan', effort: 5,
-    created: new Date('2018-08-15'), due: undefined,
-    title: 'Error in console when clicking Add',
-    },
-    {
-    id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
-    created: new Date('2018-08-16'), due: new Date('2018-08-30'),
-    title: 'Missing bottom border on panel',
-    },
-];
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+function jsonDateReviver(key, value) {
+ if (dateRegex.test(value)) return new Date(value);
+ return value;
+}
+
+async function graphQLFetch(query, variables = {}) {
+    try {
+        const response = await fetch('/graphql', {
+            method: 'POST',
+            headers: { 'Content-Type' : 'application/json'},
+            body: JSON.stringify({ query, variables })
+        });
+        const body = await response.text();
+        const result = JSON.parse(body, jsonDateReviver);
+
+        if (result.errors) {
+            const error = result.errors[0];
+            if (error.extensions.code == 'BAD_USER_INPUT') {
+                const details = error.extensions.exception.errors.join('\n ');
+                alert(`${error.message}:\n ${details}`);
+            } else {
+                alert(`${error.extensions.code}: ${error.message}`);
+            }
+        }
+        return result.data;
+    } catch(e) {
+        alert(`Error in sending data to server: ${e.message}`);
+    }
+}
 
 class IssueFilter extends React.Component {
     render() {
@@ -51,7 +69,8 @@ class IssueAdd extends React.Component {
         e.preventDefault();
         const form = document.forms.issueAdd;
         const issue = {
-            owner: form.owner.value, title: form.title.value, status: 'New'
+            owner: form.owner.value, title: form.title.value, 
+            due: new Date(new Date().getTime() + 1000*60*60*24*10),
         }
         this.props.createIssue(issue);
         form.owner.value = ""; form.title.value = "";
@@ -79,17 +98,31 @@ class IssueList extends React.Component {
         this.loadData();
     }
 
-    loadData() {
-        setTimeout(() => {this.setState({ issues: initialIssues});
-        }, 500);
+    async loadData() {
+        const query = `query {
+            issueList {
+                id title status owner 
+                created effort due
+            }
+        }`;
+
+        const data = await graphQLFetch(query);
+        if (data) {
+            this.setState({ issues: data.issueList })
+        }
     }
 
-    createIssue(issue) {
-        issue.id = this.state.issues.length + 1;
-        issue.created = new Date();
-        const newIssueList = this.state.issues.slice();
-        newIssueList.push(issue);
-        this.setState({ issues: newIssueList});
+    async createIssue(issue) {
+        const query = `mutation issueAdd($issue: IssueInputs!) {
+            issueAdd(issue: $issue) {
+            id
+            }
+        }`;
+
+        const data = await graphQLFetch(query, { issue });
+        if (data) {
+            this.loadData();
+        }
     }
     render() {
         return (
@@ -114,7 +147,7 @@ function IssueRow(props) {
                 <td>{issue.owner}</td>
                 <td>{issue.created.toDateString()}</td>
                 <td>{issue.effort}</td>
-                <td>{issue.due ? issue.due.toDateString() : ''}</td>
+                <td>{issue.due ? issue.due.toDateString() : ' '}</td>
                 <td>{issue.title}</td>
             </tr>
         );
